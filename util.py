f17336ad3ac3c6e7ddf6511b0c9db30b60930c7d
scyyc6 <scyyc6@exmail.nottingham.ac.uk>
2024-03-25 17:55:37 -0500

    organize files

---
+++ util.py
@@ -0,0 +1,4 @@
+feature_root = './indexed_dataset/'
+index_path = feature_root + "siglip-image-index.bin"
+image_path = feature_root + "siglip_image_urls.json"
+query_url = "https://static.flickr.com/2432/3801566410_bca2441029.jpg"

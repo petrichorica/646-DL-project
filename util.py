84dd483a0caed94c20523ce09434b677198145fd
RRQLiu <rl115@rice.edu>
2024-03-27 17:49:46 -0500

    1000-v3

--- util.py
+++ util.py
@@ -1,5 +1,5 @@
 feature_root = './indexed_dataset/'
-index_path = feature_root + "siglip-image-index-1000-2.bin"
-image_path = feature_root + "siglip_image_urls-1000-2.json"
+index_path = feature_root + "siglip-image-index-1000-3.bin"
+image_path = feature_root + "siglip_image_urls-1000-3.json"
 query_url = "https://static.flickr.com/2432/3801566410_bca2441029.jpg"
 train_images_path = "./dataset/SBU_captioned_photo_dataset_urls.txt"

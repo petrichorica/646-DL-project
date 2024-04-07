aea77d1162a382a566c2b5c525a1545994f0df49
scyyc6 <scyyc6@exmail.nottingham.ac.uk>
2024-04-07 01:19:39 -0500

    convert 10k-36k

--- siglip-image-indexing.py
+++ siglip-image-indexing.py
@@ -15,27 +15,29 @@ index = faiss.IndexFlatL2(extractor.shape)
 train_images = open(util.train_images_path, "r").readlines()
 #load in storage path
 feature_root = util.feature_root
+#load in start idx and end idx
+start_idx = util.start_idx
+end_idx = util.end_idx
 
 image_urls = []
 
-print("start indexing...")
+print(f"start indexing for {start_idx} to {end_idx} ...")
 start = time.time()
 
-for img_url in train_images[8000:10000]:
+for i, img_url in enumerate(train_images[util.start_idx:util.end_idx]):
     img_url = img_url.strip()
     try:
         frame = Image.open(requests.get(img_url, stream=True).raw)
     except:
-        print(f"Failed to load image {img_url}")
+        print(f"Failed to load image {i}: {img_url}")
         continue
     image_urls.append(img_url)
     embedding = extractor.get_image_embedding(frame)
     # print(embedding.shape)
     index.add(embedding)
-    # print(f"Added image {img_url} to index")
 
 end = time.time()
-print('Finish in ' + str(end - start) + ' seconds')
+print(f'Finish indexing for {start_idx} to {end_idx} in ' + str(end - start) + ' seconds')
 faiss.write_index(index, util.index_path)
 
 with open(util.image_path, "w") as f:
--- util.py
+++ util.py
@@ -1,6 +1,11 @@
-feature_root = './indexed_dataset/'
-index_path = feature_root + "siglip-image-index-0-10000.bin"
-image_path = feature_root + "siglip_image_urls-0-10000.json"
+feature_root = './indexed_30k-60k/'
+
+start_idx = 30000
+end_idx = 36000
+
+index_path = feature_root + "siglip-image-index-{}k-{}k.bin".format(start_idx//1000, end_idx//1000)
+image_path = feature_root + "siglip_image_urls-{}k-{}k.json".format(start_idx//1000, end_idx//1000)
+
 query_url = "https://static.flickr.com/2432/3801566410_bca2441029.jpg"
 train_images_path = "./dataset/SBU_captioned_photo_dataset_urls.txt"
 

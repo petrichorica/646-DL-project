925f8f6ee8361005fad6d24c452dd7341e1fc025
Jiarui <lilyliang0021@gmail.com>
2024-04-19 16:41:33 -0500

    add search_by_image

--- flask_app.py
+++ flask_app.py
@@ -4,13 +4,20 @@ import numpy as np
 import json
 from PIL import Image
 from extract_features import mySigLipModel
+import util
 
 app = Flask(__name__)
 
-index = faiss.read_index('index100k/siglip-image-index-0-100k.bin')
+# Limit file upload size to 8MB
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
+
+index_path = util.index_path
+index = faiss.read_index(index_path)
 print("Total vectors in the index:", index.ntotal)
 
-with open('index100k/siglip-image-index-0-100k.json') as f:
+image_path = util.image_path
+
+with open(image_path, "r") as f:
     image_urls = json.load(f)
 print("Number of URLs loaded:", len(image_urls))
 
@@ -26,7 +33,7 @@ def search_by_caption():
     print("Caption received:", caption)
 
     caption_embedding = extractor.get_text_embedding(caption)
-    print("Caption Embedding:", caption_embedding)
+    # print("Caption Embedding:", caption_embedding)
 
     query_vector = np.array([caption_embedding]).astype('float32').squeeze()
     if query_vector.ndim == 1:
@@ -42,6 +49,46 @@ def search_by_caption():
         'distances': distances.tolist(),
         'images': result_images
     })
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
+
+@app.route('/search_by_image', methods=['POST'])
+def search_by_image():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+    
+    file = request.files['file']
+    k = request.args.get('k', 10)
+    try:
+        k = int(k)
+    except ValueError:
+        return jsonify({'error': 'Invalid value for k'})
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+    
+    print("File received:", file.filename)
+
+    if file and allowed_file(file.filename):
+        image = Image.open(file.stream)
+        image_embedding = extractor.get_image_embedding(image)
+        query_vector = np.array([image_embedding]).astype('float32').squeeze()
+        if query_vector.ndim == 1:
+            query_vector = query_vector.reshape(1, -1)
+
+        print("Query Vector Shape:", query_vector.shape)
+
+        distances, indices = index.search(query_vector, k)
+
+        result_images = [image_urls[idx] for idx in indices[0]]
+
+        return jsonify({
+            'distances': distances.tolist(),
+            'images': result_images
+        })
+    
+    return jsonify({'error': 'Invalid file type'})
 
 
 if __name__ == '__main__':

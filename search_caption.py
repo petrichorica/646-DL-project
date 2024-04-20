4c7e6f6fe76d42a36f0bf34be951818299e22456
Jiarui <lilyliang0021@gmail.com>
2024-04-20 17:53:42 -0500

    add timing

--- search_caption.py
+++ search_caption.py
@@ -2,6 +2,7 @@ import faiss
 from extract_features import mySigLipModel
 from display_image import ImageDisplay
 import util
+import time
 
 index_path = util.index_path
 
@@ -9,16 +10,22 @@ index_path = util.index_path
 extractor = mySigLipModel()
 #load in image displayer
 display = ImageDisplay()
+#load in indexer
+index = faiss.read_index(index_path)
+
+start = time.time()
 
 #load in user query
-query = "a dog playing in the park"
+query = "temples in thailand with blue sky"
 query_embedding = extractor.get_text_embedding(query)
 
 #search based on similarity
-index = faiss.read_index(index_path)
-results_num = 5
+results_num = 10
 D, I = index.search(query_embedding, results_num)
 results = I[0]
+
+end = time.time()
+print(f"Search time: {end-start} seconds")
 
 #display the results
 print(f"Displaying search results for query: {query}")

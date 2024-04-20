a07d491be7081f770d3291181ae3d3cbae2ac685
Jiarui <lilyliang0021@gmail.com>
2024-04-20 17:53:59 -0500

    update util

--- util.py
+++ util.py
@@ -1,12 +1,19 @@
-feature_root = './indexed_60k-100k/'
+feature_root = './hnsw_index_0-100k/'
 
-start_idx = 88000
-end_idx = 100000
+start_idx = 0
+end_idx = 10000
 
-index_path = feature_root + "siglip-image-index-{}k-{}k.bin".format(start_idx//1000, end_idx//1000)
-image_path = feature_root + "siglip_image_urls-{}k-{}k.json".format(start_idx//1000, end_idx//1000)
+index = 'HNSWFlat'
+# index = 'FlatL2'
 
-query_url = "https://static.flickr.com/2432/3801566410_bca2441029.jpg"
+if (index == 'HNSWFlat'):
+    index_path = feature_root + "hnsw_index-{}k-{}k.bin".format(start_idx//1000, end_idx//1000)
+    image_path = feature_root + "hnsw_urls-{}k-{}k.json".format(start_idx//1000, end_idx//1000)
+elif (index == 'FlatL2'):
+    index_path = feature_root + "siglip-image-index-{}k-{}k.bin".format(start_idx//1000, end_idx//1000)
+    image_path = feature_root + "siglip_image_urls-{}k-{}k.json".format(start_idx//1000, end_idx//1000)
+
+query_url = "http://static.flickr.com/1126/5157409353_805483d0e4.jpg"
 train_images_path = "./dataset/SBU_captioned_photo_dataset_urls.txt"
 
 evaluation_path = "./evaluation/sample-10000/"

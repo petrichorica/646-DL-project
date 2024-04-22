fa0da8146c72c55fe953f01be2a23cad0a663855
scyyc6 <scyyc6@exmail.nottingham.ac.uk>
2024-04-22 14:16:30 -0500

    organize the files

---
+++ indexing/extract_features.py
@@ -0,0 +1,36 @@
+from transformers import AutoProcessor, AutoTokenizer, AutoModel, SiglipVisionModel
+import torch
+import torch.nn as nn
+
+class mySigLipModel(nn.Module):
+    def __init__(self):
+        super(mySigLipModel, self).__init__()
+        self.model = AutoModel.from_pretrained("google/siglip-base-patch16-224")
+        self.processor = AutoProcessor.from_pretrained("google/siglip-base-patch16-224")
+        self.tokenizer = AutoTokenizer.from_pretrained("google/siglip-base-patch16-224")
+        self.shape = 768
+
+    def get_image_embedding(self, frame):
+        image = frame.convert("RGB")
+        inputs = self.processor(images=image, return_tensors="pt")
+        outputs = self.model.get_image_features(**inputs)
+        return outputs.detach().numpy()
+    
+    def get_text_embedding(self, text):
+        inputs = self.tokenizer(text, padding="max_length", return_tensors="pt")
+        outputs = self.model.get_text_features(**inputs)
+        return outputs.detach().numpy()
+    
+class mySigLipVisionModel(nn.Module):
+    def __init__(self):
+        super(mySigLipVisionModel, self).__init__()
+        self.model = SiglipVisionModel.from_pretrained("google/siglip-base-patch16-224")
+        self.processor = AutoProcessor.from_pretrained("google/siglip-base-patch16-224")
+
+    def get_image_embedding(self, frame):
+        image = frame.convert("RGB")
+        inputs = self.processor(images=image, return_tensors="pt")
+        outputs = self.model(**inputs)
+        features = outputs.last_hidden_state
+        features = torch.flatten(features, start_dim=1)
+        return features.detach().numpy()
